fa4822476f2b3d2f8b38174710c7d8598effe5f5
Alien Shrestha <npalien4u@live.com>
2024-01-31 12:37:53 +0530

    - added widgets

---
+++ template/widgets/client-carousel-block.php
@@ -0,0 +1,78 @@
+<div class="client-carousel ">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-3">
+                <h3 class="client-carousel__title">Trusted and funded by more<br> then 800 companies</h3>
+            </div>
+            <div class="col-xl-9">
+                <div class="client-carousel__one hiredots-owl__carousel owl-theme owl-carousel" data-owl-options='{
+                    "items": 5,
+                    "margin": 65,
+                    "smartSpeed": 700,
+                    "loop":true,
+                    "autoplay": 6000,
+                    "nav":false,
+                    "dots":false,
+                    "navText": ["<span class=\"fa fa-angle-left\"></span>","<span class=\"fa fa-angle-right\"></span>"],
+                    "responsive":{
+                        "0":{
+                            "items":1,
+                            "margin": 0
+                        },
+                        "360":{
+                            "items":2,
+                            "margin": 30
+                        },
+                        "575":{
+                            "items":3,
+                            "margin": 30
+                        },
+                        "768":{
+                            "items":3,
+                            "margin": 40
+                        },
+                        "992":{
+                            "items": 4,
+                            "margin": 40
+                        },
+                        "1200":{
+                            "items": 5
+                        }
+                    }
+                    }'>
+                    <div class="client-carousel__one__item">
+                        <img src="assets/images/resources/brand-1-1.png" alt="hiredots">
+                    </div><!-- /.owl-slide-item-->
+                    <div class="client-carousel__one__item">
+                        <img src="assets/images/resources/brand-1-1.png" alt="hiredots">
+                    </div><!-- /.owl-slide-item-->
+                    <div class="client-carousel__one__item">
+                        <img src="assets/images/resources/brand-1-1.png" alt="hiredots">
+                    </div><!-- /.owl-slide-item-->
+                    <div class="client-carousel__one__item">
+                        <img src="assets/images/resources/brand-1-1.png" alt="hiredots">
+                    </div><!-- /.owl-slide-item-->
+                    <div class="client-carousel__one__item">
+                        <img src="assets/images/resources/brand-1-1.png" alt="hiredots">
+                    </div><!-- /.owl-slide-item-->
+                    <div class="client-carousel__one__item">
+                        <img src="assets/images/resources/brand-1-1.png" alt="hiredots">
+                    </div><!-- /.owl-slide-item-->
+                    <div class="client-carousel__one__item">
+                        <img src="assets/images/resources/brand-1-1.png" alt="hiredots">
+                    </div><!-- /.owl-slide-item-->
+                    <div class="client-carousel__one__item">
+                        <img src="assets/images/resources/brand-1-1.png" alt="hiredots">
+                    </div><!-- /.owl-slide-item-->
+                    <div class="client-carousel__one__item">
+                        <img src="assets/images/resources/brand-1-1.png" alt="hiredots">
+                    </div><!-- /.owl-slide-item-->
+                    <div class="client-carousel__one__item">
+                        <img src="assets/images/resources/brand-1-1.png" alt="hiredots">
+                    </div><!-- /.owl-slide-item-->
+                </div><!-- /.thm-owl__slider -->
+            </div>
+        </div>
+        <div class="client-carousel__border"></div>
+    </div><!-- /.container -->
+</div><!-- /.client-carousel -->
---
+++ template/widgets/feature-3-block.php
@@ -0,0 +1,46 @@
+<section class="about-three">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="about-three__image wow fadeInLeft" data-wow-delay="300ms">
+                    <img src="assets/images/shapes/about-3-1.png" alt="hiredots">
+                </div><!-- /.about-three__image -->
+            </div><!-- /.col-lg-6 -->
+            <div class="col-lg-6 wow fadeInRight" data-wow-delay="300ms">
+                <div class="about-three__content">
+                    <div class="sec-title text-left">
+
+                        <h6 class="sec-title__tagline">About our agency</h6><!-- /.sec-title__tagline -->
+
+                        <h3 class="sec-title__title">Reliable & Cost Efficiant Recruitment Agency</h3><!-- /.sec-title__title -->
+                    </div><!-- /.sec-title -->
+                    <p class="about-three__content__text">
+                    Welcome to Bua & Miracle HR Consultancy L.L.C., a member of the Bu Abdullah Group of Companies and an emerging HR consulting firm based in Dubai, UAE. We specialize in providing exceptional Recruitment and HR Services to startups, established companies, and diverse industries across the Middle East, East Asia, Europe, Canada, Australia, New Zealand, and beyond.
+                    </p>
+                    <ul class="about-three__list">
+                        <li><span class="fa fa-check-circle"></span>Elevating talents, powering businesses</li>
+                        <li><span class="fa fa-check-circle"></span>Exceptional HR solutions for global growth and success</li>
+                        <li><span class="fa fa-check-circle"></span>Bridging talent gaps, shaping organizational excellence seamlessly</li>
+                    </ul>
+                    <div class="row">
+                        <div class="col-md-7">
+                            <a class="about-three__call" href="tel:92003680900">
+                                <span class="about-three__call__icon"><i class="icon-calling"></i></span>
+                                <span class="about-three__call__text">Call to Questions</span>
+                                <span class="about-three__call__number">+(971) 521 984 592</span>
+                            </a>
+                            <a href="about" class="hiredots-btn hiredots-btn--base"><span>Discover more</span></a>
+                        </div>
+                        <div class="col-md-5">
+                            <div class="about-three__info-box">
+                                <div class="about-three__info-box__icon"><span class="icon-cooperation"></span></div>
+                                <h3 class="about-three__info-box__title">Trusted by Clients</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+        <div class="about-three__border"></div>
+    </div><!-- /.container -->
+</section><!-- /.about-three -->
