f6ac8613b88ad6e2279809ad73a0c901b732346e
Alien Shrestha <npalien4u@live.com>
2024-01-31 13:05:53 +0530

    - testimonials section updated

---
+++ template/widgets/project-1-block.php
@@ -0,0 +1,102 @@
+
+<div class="project-one project-one--home-three">
+    <div class="container">
+        <div class="sec-title text-center">
+
+            <h6 class="sec-title__tagline">Global Workforce Solutions</h6><!-- /.sec-title__tagline -->
+
+            <h3 class="sec-title__title">Sending Talent Across Borders</h3><!-- /.sec-title__title -->
+        </div><!-- /.sec-title -->
+        <div class="project-one__carousel hiredots-owl__carousel hiredots-owl__carousel--with-shadow hiredots-owl__carousel--basic-nav owl-carousel owl-theme" data-owl-options='{
+			"items": 1,
+			"margin": 30,
+			"loop": false,
+			"smartSpeed": 700,
+			"nav": false,
+			"navText": ["<span class=\"fa fa-angle-left\"></span>","<span class=\"fa fa-angle-right\"></span>"],
+			"dots": false,
+			"autoplay": false,
+			"responsive": {
+				"0": {
+					"items": 1
+				},
+				"576": {
+					"items": 2
+				},
+				"992": {
+					"items": 3
+				},
+				"1360": {
+					"items": 4
+				}
+			}
+			}'>
+            <div class="item">
+                <div class="project-one__card wow fadeInUp" data-wow-duration='1500ms' data-wow-delay='1ms'>
+                    <div class="project-one__img">
+                        <img src="assets/images/project/project-1-1.jpg" alt="Staffing Solution">
+                    </div>
+                    <div class="project-one__content">
+                        <span class="project-one__tagline">Resources</span>
+                        <h3 class="project-one__title"><a href="project-details.html">UAE</a></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="project-one__card wow fadeInUp" data-wow-duration='1500ms' data-wow-delay='1ms'>
+                    <div class="project-one__img">
+                        <img src="assets/images/project/project-1-2.jpg" alt="Staffing Solution">
+                    </div>
+                    <div class="project-one__content">
+                        <span class="project-one__tagline">Resources</span>
+                        <h3 class="project-one__title"><a href="project-details.html">Middle East</a></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="project-one__card wow fadeInUp" data-wow-duration='1500ms' data-wow-delay='1ms'>
+                    <div class="project-one__img">
+                        <img src="assets/images/project/project-1-3.jpg" alt="Staffing Solution">
+                    </div>
+                    <div class="project-one__content">
+                        <span class="project-one__tagline">Resources</span>
+                        <h3 class="project-one__title"><a href="project-details.html">Europe</a></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="project-one__card wow fadeInUp" data-wow-duration='1500ms' data-wow-delay='1ms'>
+                    <div class="project-one__img">
+                        <img src="assets/images/project/project-1-4.jpg" alt="Staffing Solution">
+                    </div>
+                    <div class="project-one__content">
+                        <span class="project-one__tagline">Resources</span>
+                        <h3 class="project-one__title"><a href="project-details.html">Canada</a></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="project-one__card wow fadeInUp" data-wow-duration='1500ms' data-wow-delay='1ms'>
+                    <div class="project-one__img">
+                        <img src="assets/images/project/project-1-5.jpg" alt="Staffing Solution">
+                    </div>
+                    <div class="project-one__content">
+                        <span class="project-one__tagline">Resources</span>
+                        <h3 class="project-one__title"><a href="project-details.html">Australia</a></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="project-one__card wow fadeInUp" data-wow-duration='1500ms' data-wow-delay='1ms'>
+                    <div class="project-one__img">
+                        <img src="assets/images/project/project-1-6.jpg" alt="Staffing Solution">
+                    </div>
+                    <div class="project-one__content">
+                        <span class="project-one__tagline">Resources</span>
+                        <h3 class="project-one__title"><a href="project-details.html">East Asia</a></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ template/widgets/testimonials-block.php
@@ -0,0 +1,69 @@
+
+<section class="testimonials-three">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="sec-title text-left">
+
+                    <h6 class="sec-title__tagline">Our Testimonials</h6><!-- /.sec-title__tagline -->
+
+                    <h3 class="sec-title__title">What They’re Talking<br> About Bua & Miracle HR Consultancy?</h3><!-- /.sec-title__title -->
+                </div><!-- /.sec-title -->
+                <div class="testimonials-three__carousel hiredots-owl__carousel owl-carousel" data-owl-options='{
+						"items": 1,
+						"margin": 0,
+						"loop": true,
+						"smartSpeed": 700,
+						"nav": true,
+						"navText": ["<span class=\"icon-left-arrow\"></span>","<span class=\"icon-right-arrow\"></span>"],
+						"dots": false,
+						"autoplay": false
+					}'>
+                    <div class="item">
+                        <div class="testimonials-three__item">
+                            <div class="testimonials-three__ratings">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <div class="testimonials-three__content">
+                            "Working with Bua & Miracle HR Consultancy was an absolute game-changer for our company. Their team went above and beyond to understand our hiring needs and provided us with top-tier candidates who fit seamlessly into our organization. Their professionalism, attention to detail, and commitment to excellence truly set them apart. We couldn't be happier with the results!" 
+                            </div>
+                            <div class="testimonials-three__meta">
+                                <h5 class="testimonials-three__meta__name">Sarah Thompson</h5><span class="testimonials-three__meta__dot"></span>CEO, Tech Innovations Inc
+                            </div>
+                        </div>
+                    </div><!-- /.item -->
+                    <div class="item">
+                        <div class="testimonials-three__item">
+                            <div class="testimonials-three__ratings">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <div class="testimonials-three__content">
+                            "Bua & Miracle HR Consultancy has been instrumental in helping us navigate the complexities of international recruitment. Their expertise in sourcing and placing talent across borders has been invaluable to our expansion efforts. From start to finish, their team provided exceptional service, guiding us through every step of the process with efficiency and professionalism. We highly recommend their services to any company looking to build a global workforce."
+                            </div>
+                            <div class="testimonials-three__meta">
+                                <h5 class="testimonials-three__meta__name">John Smith</h5><span class="testimonials-three__meta__dot">.</span>HR Manager, Global Solutions Ltd
+                            </div>
+                        </div>
+                    </div><!-- /.item -->
+                </div><!-- /.testimonials-three__carousel -->
+            </div>
+            <div class="col-lg-6">
+                <div class="testimonials-three__thumbs">
+                    <div class="testimonials-three__thumbs__item"><img src="assets/images/resources/testi-3-1.jpg" alt="hiredots"></div>
+                    <div class="testimonials-three__thumbs__item"><img src="assets/images/resources/testi-3-2.jpg" alt="hiredots"></div>
+                    <div class="testimonials-three__thumbs__item"><img src="assets/images/resources/testi-3-3.jpg" alt="hiredots"></div>
+                    <div class="testimonials-three__thumbs__item"><img src="assets/images/resources/testi-3-4.jpg" alt="hiredots"></div>
+                    <div class="testimonials-three__thumbs__icon"><img src="assets/images/shapes/testimonial-icon.png" alt="hiredots"></div>
+                </div>
+            </div>
+        </div>
+    </div><!-- /.container -->
+</section><!-- /.testimonials-three -->
