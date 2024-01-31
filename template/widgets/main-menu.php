55d70372dac06b891f2bc040d4be6a9470bd7fff
Alien Shrestha <npalien4u@live.com>
2024-01-31 13:55:25 +0530

    - incomplete links disabled

--- template/widgets/main-menu.php
+++ template/widgets/main-menu.php
@@ -6,26 +6,26 @@
         <li class="dropdown">
             <a href="#">Company</a>
             <ul>
-                <li><a href="messsage-from-chairmain">Message from Chairman</a></li>
-                <li><a href="mission-vision-goal">Mission Vision Goal</a></li>
-                <li><a href="our-team">Our Team</a></li>
-                <li><a href="why-us">Why Us</a></li>
-                <li><a href="company-profile">Company Profile</a></li>
+                <li><a href="#">Message from Chairman</a></li>
+                <li><a href="#">Mission Vision Goal</a></li>
+                <li><a href="#">Our Team</a></li>
+                <li><a href="#">Why Us</a></li>
+                <li><a href="#">Company Profile</a></li>
             </ul>
         </li>
         <li class="dropdown">
             <a href="#">Servcies</a>
             <ul>
-                <li><a href="hr-consultancy-uae">HR Consultancy in UAE</a></li>
-                <li><a href="overseas-recruitment-services">Overseas Recruitment Services</a></li>
-                <li><a href="global-reach">Global Reach</a></li>
-                <li><a href="staffing-solutions">Why Us</a></li>
-                <li><a href="company-profile">Company Profile</a></li>
+                <li><a href="#">HR Consultancy in UAE</a></li>
+                <li><a href="#">Overseas Recruitment Services</a></li>
+                <li><a href="#">Global Reach</a></li>
+                <li><a href="#">Why Us</a></li>
+                <li><a href="#">Company Profile</a></li>
             </ul>
         </li>
-        <li><a href="industries">Industries</a></li>
-        <li><a href="jobs">Jobs</a></li>
-        <li><a href="testimonials">Testimonials</a></li>
+        <li><a href="#">Industries</a></li>
+        <li><a href="#">Jobs</a></li>
+        <li><a href="#">Testimonials</a></li>
         <li><a href="contact">Contact</a></li>
     </ul>
 </nav><!-- /.main-header__nav -->
