d68519adbca3de519af00596a453f0c84c786248
Alien Shrestha <npalien4u@live.com>
2024-01-31 10:35:43 +0530

    - created simple tempate engine

---
+++ template/layout.php
@@ -0,0 +1,7 @@
+<?php   
+    include "header.php";
+
+    echo $contents;
+
+    include "footer.php";
+?>
---
+++ template/widgets/main-menu.php
@@ -0,0 +1,25 @@
+<nav class="main-header__nav main-menu">
+    <ul class="main-menu__list">
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li class="dropdown">
+            <a href="#">Company</a>
+            <ul>
+                <li><a href="team-details">Team Details</a></li>
+                <li><a href="jobs">Jobs</a></li>
+                <li><a href="history">History</a></li>
+                <li><a href="faq">FAQs</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="services">Servies</a>
+        </li>
+        <li>
+            <a href="index">Jobs</a>
+        </li>
+        <li>
+            <a href="contact">Contact</a>
+        </li>
+    </ul>
+</nav><!-- /.main-header__nav -->
