b1d6308dbb7ce65acc2d09b363084003fe98e73b
Alien Shrestha <npalien4u@live.com>
2024-02-23 15:00:01 +0530

    - rearrange and redsign org profile block

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 $widgets = [
     "home-page-banner", "feature-2-block",
     "feature-3-block", "service-2-block",
-    "job-category-block", "home-page-jobs-list",
+    "job-category-block", "home-page-jobs-list","organization-profile",
     "faq-block", "project-1-block",
-    "team-1-block","clients-slider", "organization-profile", "footer-map"
+    "team-1-block","clients-slider",  "footer-map"
 ];
 
 foreach ($widgets as $widget) {
