3c3a50600b7bc508d17728b9eb99e7a4215d9e5b
Alien Shrestha <npalien4u@live.com>
2024-02-23 15:41:55 +0530

    - profile section css fix for mobile view

--- template/widgets/organization-profile.php
+++ template/widgets/organization-profile.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-6 ">
                 <section class="row p-2">
-                    <div class="col-lg-6 bg-white p-2">
+                    <div class="col-lg-6 bg-white p-4">
                         <div class="team-details__image ">
                             <img src="assets/images/team/bijaya-karki-uae-2.png">
                         </div><!-- /.team-details__image -->
@@ -23,7 +23,7 @@
                         </ul><!-- /.list-unstyled team-details__list -->
                     </div><!-- /.col-lg-6 -->
                     <div class="col-lg-6 bg-white px-0">
-                        <div class="team-details__text text-center text-dark lh-base p-2 ">
+                        <div class="team-details__text text-center text-dark lh-base p-4 ">
                             <p><strong>Dear Esteemed Clients,</strong></p>
                             <p>I am delighted to extend a warm welcome to you as the Managing Director of Bua & Miracle HR Consultancy. It is an honor to havet he opportunity to serve you and assist in fulfilling your HR needs.</p>
                             <p>At Bua & Miracle HR Consultancy, we are driven by a passion for excellence and a commitment to providing exceptional HR solutions. Our goal is to be your trusted partner, assisting you in navigating the ever-evolving talent landscape and achieving your organizational objectives.</p>
@@ -33,20 +33,20 @@
                 </section><!-- /.team-details -->
             </div>
 
-            <div class="col-md-6 p-2">
-                <div class="row mb-2 mx-0">
-                    <div class="col-md-4 bg-white">
+            <div class="col-md-6 ">
+                <div class="row mb-2 mx-0 py-2">
+                    <div class="col-md-4 p-3 bg-white ">
                         <div class="team-details__image align-middle">
-                            <img class="mt-5" src="assets/images/company/bua-miracle150-1.png">
+                            <img src="assets/images/company/bua-miracle150-1.png">
                         </div>
                     </div>
                     <div class="col-md-8 ">
 
-                        <div class="row bg-white p-2 ms-1">
+                        <div class="row bg-white p-3">
                             <div class="col-md-5 p-0">
                                 <img src="assets/images/resources/profile/bua-miracle-profile.png">
                             </div>
-                            <div class="col-md-7  d-flex flex-column">
+                            <div class="col-md-7 d-flex flex-column">
                                 <a href="//manpowermiracle.com" class="btn btn-warning  mb-2">Visit Website</a>
                                 <a href="//manpowermiracle.com" target="_blank" class="btn btn-danger mb-2 ">Company Profile</a>
                             </div>
@@ -56,19 +56,19 @@
                     </div>
                 </div>
 
-                <div class="row mb-2 mx-0">
+                <div class="row mb-2 mx-0 py-2">
                     <div class="col-md-4 bg-white">
                         <div class="team-details__image">
-                            <img class="mt-5" src="assets/images/company/miracle-manpower150-1.png">
+                            <img src="assets/images/company/miracle-manpower150-1.png">
                         </div>
                     </div>
                     <div class="col-md-8 ">
 
-                        <div class="row bg-white p-2 ms-1">
+                        <div class="row bg-white p-3">
                             <div class="col-md-5 p-0">
                                 <img src="assets/images/resources/profile/miracle-nepal-profile.png">
                             </div>
-                            <div class="col-md-7 ps-4 d-flex flex-column">
+                            <div class="col-md-7 d-flex flex-column align-middle">
                                 <a href="//manpowermiracle.com" class="btn btn-warning mb-2">Visit Website</a>
                                 <a href="//manpowermiracle.com" target="_blank" class="btn btn-danger mb-2">Company Profile</a>
                             </div>
