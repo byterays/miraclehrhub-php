39bbc72afe8fe8bea0556a41e166e3d248b4140b
Alien Shrestha <npalien4u@live.com>
2024-02-23 10:46:54 +0530

    - profile section created

---
+++ template/widgets/organization-profile.php
@@ -0,0 +1,82 @@
+<div class="container bg-warning  mb-5">
+    <div class="row">
+        <div class="col-md-6 ">
+            <section class="row p-2">
+                <div class="col-lg-6 bg-white p-2">
+                    <div class="team-details__image ">
+                        <img src="assets/images/team/bijaya-karki-uae-2.png">
+                    </div><!-- /.team-details__image -->
+                    <ul class="list-unstyled text-center ">
+                        <li>
+                            <strong>Bijaya Karki, Managing Director</strong>
+                        </li>
+                        <li>
+                            Miracle Manpower (Nepal)
+                        </li>
+                        <li>
+                            Bua & Miracle HR Consultancies (UAE)
+                        </li>
+                        <li>
+                            Miracle Management (Malaysia)
+                        </li>
+                    </ul><!-- /.list-unstyled team-details__list -->
+                </div><!-- /.col-lg-6 -->
+                <div class="col-lg-6 bg-white px-0">
+                    <div class="team-details__text text-center text-dark lh-base p-2 ">
+                        <p><strong>Dear Esteemed Clients,</strong></p>
+                        <p>I am delighted to extend a warm welcome to you as the Managing Director of Bua & Miracle HR Consultancy. It is an honor to havet he opportunity to serve you and assist in fulfilling your HR needs.</p>
+                        <p>At Bua & Miracle HR Consultancy, we are driven by a passion for excellence and a commitment to providing exceptional HR solutions. Our goal is to be your trusted partner, assisting you in navigating the ever-evolving talent landscape and achieving your organizational objectives.</p>
+                    </div>
+                    <!-- /.team-details__text -->
+                </div><!-- /.col-lg-6 -->
+            </section><!-- /.team-details -->
+        </div>
+
+        <div class="col-md-6 p-2">
+            <div class="row mb-2 mx-0">
+                <div class="col-md-4 bg-white">
+                    <div class="team-details__image align-middle">
+                        <img  class="mt-5"  src="assets/images/company/bua-miracle150-1.png">
+                    </div>
+                </div>
+                <div class="col-md-8 ">
+
+                    <div class="row bg-white p-2 ms-1">
+                        <div class="col-md-5 p-0">
+                            <img src="assets/images/resources/profile/bua-miracle-profile.png">
+                        </div>
+                        <div class="col-md-7 ps-4 d-flex flex-column">
+                            <a href="//manpowermiracle.com" class="btn btn-warning  mb-2">Visit Website</a>
+                            <a href="//manpowermiracle.com" target="_blank" class="btn btn-danger mb-2 ">Company Profile</a>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row mb-2 mx-0">
+                <div class="col-md-4 bg-white">
+                    <div class="team-details__image">
+                        <img class="mt-5" src="assets/images/company/miracle-manpower150-1.png">
+                    </div>
+                </div>
+                <div class="col-md-8 ">
+
+                    <div class="row bg-white p-2 ms-1">
+                        <div class="col-md-5 p-0">
+                            <img src="assets/images/resources/profile/miracle-nepal-profile.png">
+                        </div>
+                        <div class="col-md-7 ps-4 d-flex flex-column">
+                            <a href="//manpowermiracle.com" class="btn btn-warning mb-2">Visit Website</a>
+                            <a href="//manpowermiracle.com" target="_blank" class="btn btn-danger mb-2">Company Profile</a>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div><!-- /.container -->
