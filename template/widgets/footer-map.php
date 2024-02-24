c547fbd8f3db488cf6fc3bad0dc53c5bf828fbe5
Alien Shrestha <npalien4u@live.com>
2024-02-24 09:20:58 +0530

    - css fix

--- template/widgets/footer-map.php
+++ template/widgets/footer-map.php
@@ -1,4 +1,4 @@
-<section class="contact-map contact-map--home-three">
+<section class="contact-map contact-map--home-three p-0 m-0">
     <div class="google-map google-map__contact">
         
         <iframe title="Miracle HR Hub Location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.3187704361876!2d55.33360847449136!3d25.25986002916143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5d5cef7ee8cf%3A0x1fc94c378ac91af2!2sBu%20Abdullah%20Investment%20Group!5e0!3m2!1sen!2sin!4v1706687219721!5m2!1sen!2sin" class="map__contact" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
