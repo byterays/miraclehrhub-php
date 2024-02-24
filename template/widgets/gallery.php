8941ba1cbdae98746a7fbd3796ee2e993a2b0cb0
Alien Shrestha <npalien4u@live.com>
2024-02-24 09:35:43 +0530

    - gallery redone

--- template/widgets/gallery.php
+++ template/widgets/gallery.php
@@ -8,14 +8,21 @@
 
             $root = dirname(__DIR__, 2); // Change the number to adjust the level of directory hierarchy.
             $directory = "{$root}/assets/images/gallery/";
-           
 
-            $gallery_pics = scandir($directory);
+
+            $gallery_pics = [
+                "miracle-gallery 11.jpg", "miracle-gallery 12.jpg", "miracle-gallery 13.jpg",
+                "miracle-gallery 14.jpg", "miracle-gallery1.jpg", "miracle-gallery10.jpg",
+                "miracle-gallery2.jpg", "miracle-gallery3.jpg", "miracle-gallery4.jpg",
+                "miracle-gallery5.jpg", "miracle-gallery6.jpg", "miracle-gallery7.jpg",
+                "miracle-gallery8.jpg", "miracle-gallery9.jpg"
+            ];
+
 
 
             foreach ($gallery_pics as $image) :
 
-                if (!is_dir($directory . '/' . $image)) :
+              
             ?>
                     <div class="col-xs-6 col-md-4 col-lg-3">
 
@@ -26,7 +33,7 @@
 
                     </div>
             <?php
-                endif;
+              
             endforeach;
             ?>
         </div>
