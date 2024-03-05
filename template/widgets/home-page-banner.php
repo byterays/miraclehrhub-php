e639f00706171c5d70914c4967008297cbfc98c4
Alien Shrestha <npalien4u@live.com>
2024-03-05 11:01:06 +0530

    - updated banner content

--- template/widgets/home-page-banner.php
+++ template/widgets/home-page-banner.php
@@ -1,3 +1,31 @@
+<?php
+$banners = [
+    [
+        "banner-image"=>"assets/images/backgrounds/slider-3-1.jpg",
+        "overlay-image"=>"assets/images/backgrounds/slider-overlay.png",
+        "title" => "HR CONSULTANCY, RECRUITING AND PLACEMENT",
+        "sub-title" => "Connecting Talent with Opportunity",
+        "button"=>["text"=>"Discover More", "link"=>"overseas-recruitment-service"]
+    ],
+    [
+        "banner-image"=>"assets/images/backgrounds/slider-3-2.jpg",
+        "overlay-image"=>"assets/images/backgrounds/slider-overlay.png",
+        "title" => "YOUR PARTNER FOR OVERSEAS RECRUITMENT",
+        "sub-title" => "Empowering Your Global Workforce",
+        "button"=>["text"=>"Discover More", "link"=>"overseas-recruitment-service"]
+    ],
+    [
+        "banner-image"=>"assets/images/backgrounds/slider-3-3.jpg",
+        "overlay-image"=>"assets/images/backgrounds/slider-overlay.png",
+        "title" => "UNDERSTANDING YOUR UNIQUE NEEDS",
+        "sub-title" => "Tailored Solutions for Your Success Journey",
+        "button"=>["text"=>"Discover More", "link"=>"overseas-recruitment-service"]
+    ]
+
+
+];
+?>
+
 <section class="main-slider-three">
     <div class="main-slider-three__carousel hiredots-owl__carousel owl-carousel" data-owl-options='{
 		"loop": true,
@@ -12,19 +40,21 @@
 		"dots": false,
 		"margin": 0
 	    }'>
+
+        <?php foreach($banners as $banner): ?>
         <div class="item">
             <div class="main-slider-three__item">
-                <div class="main-slider-three__bg" style="background-image: url(assets/images/backgrounds/slider-3-1.jpg);"></div>
-                <div class="main-slider-three__overlay" style="background-image: url(assets/images/shapes/slider-overlay.png);"></div>
+                <div class="main-slider-three__bg" style="background-image: url(<?=$banner["banner-image"]?>);"></div>
+                <div class="main-slider-three__overlay" style="background-image: url(<?=$banner["overlay-image"]?>);"></div>
                 <div class="main-slider-three__shape"></div>
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
                             <div class="main-slider-three__content">
-                                <h2 class="main-slider-three__title">HR CONSULTANCY, RECRUITING<br/> AND PLACEMENT</h2><!-- slider-title -->
-                                <p class="main-slider-three__text">Solving Recruitment Using Technology</p><!-- slider-text -->
+                                <h2 class="main-slider-three__title"><?=$banner["title"]?></h2><!-- slider-title -->
+                                <p class="main-slider-three__text"><?=$banner["sub-title"]?></p><!-- slider-text -->
                                 <div class="main-slider-three__btn">
-                                    <a href="overseas-recruitment-service" class="hiredots-btn"><span>Discover More</span></a><!-- slider-btn -->
+                                    <a href="<?=$banner["button"]["link"]?>" class="hiredots-btn"><span><?=$banner["button"]["text"]?></span></a><!-- slider-btn -->
                                 </div>
                             </div>
                         </div>
@@ -32,46 +62,7 @@
                 </div>
             </div>
         </div>
-        <div class="item">
-            <div class="main-slider-three__item">
-                <div class="main-slider-three__bg" style="background-image: url(assets/images/backgrounds/slider-3-2.jpg);"></div>
-                <div class="main-slider-three__overlay" style="background-image: url(assets/images/shapes/slider-overlay.png);"></div>
-                <div class="main-slider-three__shape"></div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="main-slider-three__content">
-                                <h2 class="main-slider-three__title">YOUR PARTNER FOR<br/> OVERSEAS RECRUITMENT</h2><!-- slider-title -->
-                                <p class="main-slider-three__text">Solving Recruitment Using Technology</p><!-- slider-text -->
-                                <div class="main-slider-three__btn">
-                                    <a href="miracle-hrhub-staffing-solution" class="hiredots-btn"><span>Discover More</span></a><!-- slider-btn -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <div class="main-slider-three__item">
-                <div class="main-slider-three__bg" style="background-image: url(assets/images/backgrounds/slider-3-3.jpg);"></div>
-                <div class="main-slider-three__overlay" style="background-image: url(assets/images/shapes/slider-overlay.png);"></div>
-                <div class="main-slider-three__shape"></div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="main-slider-three__content">
-                                <h2 class="main-slider-three__title">YOUR PARTNER FOR<br/> OVERSEAS RECRUITMENT</h2><!-- slider-title -->
-                                <p class="main-slider-three__text">Solving Recruitment Using Technology</p><!-- slider-text -->
-                                <div class="main-slider-three__btn">
-                                    <a href="miracle-hrhub-staffing-solution" class="hiredots-btn"><span>Discover More</span></a><!-- slider-btn -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+       <?php endforeach;?>
     </div>
 </section>
 <!-- main-slider-end -->
